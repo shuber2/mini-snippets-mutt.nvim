c2e7014ad9fa98298e2f0dff2870cf3f4d7f2458
Stefan Huber <shuber@sthu.org>
2025-04-26 20:20:28 +0200

    Refactor code
    Follow suggestion of echasnovski [1] to reduce code nesting level.
    
    [1]  https://github.com/echasnovski/mini.nvim/discussions/1763

--- snippets/mail/mutt.lua
+++ snippets/mail/mutt.lua
@@ -1,36 +1,48 @@
 local config = require("mini-snippets-mutt").config
 local snippets = {}
 
--- Signature snippets
-for _, pattern in ipairs(config.signature_files) do
-    for _, file in ipairs(vim.fn.glob(pattern, false, true)) do
-        local filename = vim.fn.fnamemodify(file, ':t:r')
-        local body = vim.fn.readfile(file)
-        snippets[filename] = {
-            prefix = 'sig-' .. filename,
-            body = config.signature_prefix .. table.concat(body, '\n')
-        }
+
+local process_snippet_file = function(file)
+    local filename = vim.fn.fnamemodify(file, ':t:r')
+    local body = vim.fn.readfile(file)
+    snippets[filename] = {
+        prefix = 'sig-' .. filename,
+        body = config.signature_prefix .. table.concat(body, '\n')
+    }
+end
+
+local process_muttrc_line = function(line)
+    if not line:match('^set from') then return end
+
+    local from = line:match('^set from%s*=%s*"(.*)"')
+    if not from then return end
+
+    local domain = from:match('@(.*)>')
+    if not domain then return end
+
+    snippets['from-' .. domain] = {
+        prefix = 'from-' .. domain,
+        body = from
+    }
+end
+
+local process_muttrc_file = function(file)
+    local lines = vim.fn.readfile(file)
+    for _, line in ipairs(lines) do
+        process_muttrc_line(line)
     end
 end
 
--- From address snippets
+
+for _, pattern in ipairs(config.signature_files) do
+    for _, file in ipairs(vim.fn.glob(pattern, false, true)) do
+        process_snippet_file(file)
+    end
+end
+
 for _, pattern in ipairs(config.muttrc_files) do
     for _, file in ipairs(vim.fn.glob(pattern, false, true)) do
-        local lines = vim.fn.readfile(file)
-        for _, line in ipairs(lines) do
-            if line:match('^set from') then
-                local from = line:match('^set from%s*=%s*"(.*)"')
-                if from then
-                    local domain = from:match('@(.*)>')
-                    if domain then
-                        snippets['from-' .. domain] = {
-                            prefix = 'from-' .. domain,
-                            body = from
-                        }
-                    end
-                end
-            end
-        end
+        process_muttrc_file(file)
     end
 end
 
